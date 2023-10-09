995f03304661cb1661f58abd537fdb0733588846
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-09 19:13:06 +0000

    Update note.js

--- Task 1.4/note.js
+++ Task 1.4/note.js
@@ -1,8 +1,8 @@
 let num = 9;
 
-if(num % 2){
-    console.log("ƏDƏD TƏKDİR!!!")
+if(num % 2 ===0){
+    console.log("ƏDƏD CÜTDÜR!!!")
 }
 else{
-    console.log("ƏDƏD CÜTDÜR!!!")
+    console.log("ƏDƏD TƏKDİR!!!")
 }
